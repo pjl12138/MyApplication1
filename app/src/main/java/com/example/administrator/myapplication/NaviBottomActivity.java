be10387c076746dd412678818e91bf1728de4544
pjl12138 <969301263@qq.com>
2017-04-14 09:36:49 +0800

    底部导航--微信

---
+++ app/src/main/java/com/example/administrator/myapplication/NaviBottomActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NaviBottomActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navi_bottom);
+    }
+}
